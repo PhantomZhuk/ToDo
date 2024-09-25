5227434b55d34a666ee58d4fefcd7b90fe1fbc3c
Maksym Zhuk <bitmaksum550@gmail.com>
2024-09-25 21:14:46 +0300

    add start files

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require(`express`);
+const mongoose = require(`mongoose`);
+require(`dotenv`).config();
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(`public`));
+
+app.listen(PORT, () => {
+    console.log(`server run on port ${PORT}`);
+})
