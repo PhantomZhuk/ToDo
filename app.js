ab3a25dfc48949660f430d0bd0da48699a1535ef
Maksym Zhuk <bitmaksum550@gmail.com>
2024-09-25 21:21:48 +0300

    add files to the public folder

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(`public`));
 
+app.get(`/`, (req, res) => {
+    res.sendFile(__dirname, `public`, `index.html`);
+})
+
 app.listen(PORT, () => {
     console.log(`server run on port ${PORT}`);
 })
