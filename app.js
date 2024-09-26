3f9773ca67c001fb89e94f549e92bc5eebe4a1df
Maksym Zhuk <bitmaksum550@gmail.com>
2024-09-26 08:41:54 +0300

    add backend code

--- app.js
+++ app.js
@@ -12,6 +12,63 @@ app.get(`/`, (req, res) => {
     res.sendFile(__dirname, `public`, `index.html`);
 })
 
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => {
+    console.log(`MongoDB connected`);
+})
+
+const taskSchema = new mongoose.Schema({
+    title: String,
+    completed: Boolean
+})
+
+const Task = mongoose.model("Task", taskSchema);
+
+app.post("/api/tasks", (req, res) => {
+    console.log(req.body)
+    const newTask = new Task ({
+        title: req.body.title,
+        completed: req.body.completed
+    })
+
+    newTask.save();
+    res.json(newTask);
+})
+
+app.get(`/`, (req, res) => {
+    res.sendFile(__dirname , "public", "/index.html");
+})
+
+app.get(`/api/tasks`, (req, res) => {
+    Task.find()
+    .then((tasks) => {
+        res.json(tasks);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+})
+
+app.delete(`/app/tasks/:id`, (req, res) => {
+    Task.findByIdAndDelete(req.params.id)
+    .then(() => {
+        res.json({message: "Task deleted"});
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+})
+
+app.put(`/api/tasks/:id`, (req, res) => {
+    Task.findByIdAndUpdate(req.params.id, req.body)
+    .then(() => {
+        res.json({message: "Task updated"});
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+})
+
 app.listen(PORT, () => {
     console.log(`server run on port ${PORT}`);
 })
